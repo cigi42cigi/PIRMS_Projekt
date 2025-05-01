96dad3e15152abecc23a1b278a225cc966e5dd2f
Cigi <honza.ciganek@email.cz>
2025-05-01 11:51:39 +0200

    FIR filter & Signal Plotter

---
+++ Projekt/UI/SignalPlotter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.DataVisualization.Charting;
+
+namespace Projekt.UI
+{
+    class SignalPlotter
+    {
+        private readonly Chart chart;
+
+        public SignalPlotter(Chart targetChart)
+        {
+            chart = targetChart;
+            if (chart.Series.Count == 0)
+            {
+                var series = new Series
+                {
+                    ChartType = SeriesChartType.Line,
+                    Name = "Signal"
+                };
+                chart.Series.Add(series);
+            }
+        }
+
+        public void Plot(double[] x, double[] y, string seriesName)
+        {
+            var series = chart.Series[0];
+            series.Points.Clear();
+
+            for (int i = 0; i < x.Length && i < y.Length; i++)
+            {
+                series.Points.AddXY(x[i], y[i]);
+            }
+
+            if (!string.IsNullOrEmpty(seriesName))
+                series.Name = seriesName;
+
+            chart.ChartAreas[0].RecalculateAxesScale();
+        }
+
+        public void SetAxisLabels(string xLabel, string yLabel)
+        {
+            chart.ChartAreas[0].AxisX.Title = xLabel;
+            chart.ChartAreas[0].AxisY.Title = yLabel;
+        }
+    }
+}
---
+++ Projekt/filters/FIR_filter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.filters
+{
+    class FIR_filter
+    {
+        public double[] Coefficients { get; private set; }
+
+        public FIR_filter(int order, double sampleRate, double cutoffFrequency)
+        {
+            Coefficients = DesignLowPass(order, sampleRate, cutoffFrequency);
+        }
+
+        private double[] DesignLowPass(int N, double Fs, double fc)
+        {
+            double f = fc / Fs;
+            double[] h = new double[N + 1];
+            int M = N / 2;
+
+            for (int n = 0; n <= N; n++)
+            {
+                double x = n - M;
+                double sinc = x == 0 ? 2 * Math.PI * f : Math.Sin(2 * Math.PI * f * x) / (x * Math.PI);
+                double window = 0.54 - 0.46 * Math.Cos(2 * Math.PI * n / N); // Hammingovo okno
+                h[n] = sinc * window;
+            }
+
+            return h;
+        }
+
+        public double[] Apply(double[] input)
+        {
+            int N = Coefficients.Length;
+            double[] output = new double[input.Length];
+
+            for (int n = 0; n < input.Length; n++)
+            {
+                double y = 0.0;
+                for (int k = 0; k < N; k++)
+                {
+                    if (n - k >= 0)
+                        y += Coefficients[k] * input[n - k];
+                }
+                output[n] = y;
+            }
+
+            return output;
+        }
+    }
+}
